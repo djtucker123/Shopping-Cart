3a8ded6c2b16ab1cb3d2f48d8d65ca0cd1b57a92
djtucker123 <212443631@logon.ds.ge.com>
2018-10-28 17:39:14 -0400

    adding to cart and to cart array working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,19 @@
 $(document).ready(function() {
   // Here we are provided an initial array of projects.
   // Use this array to dynamically create buttons on the screen.
-  var projectList = [
-    'MagnaLab Refactor',
-    'MagnaLab Expansion',
-    'GitGame Discovery Phase',
-    'GitGame Project Meeting',
-    'GitGame Prototype',
-    'Delthon Security Check',
-    'First Bank Security Check',
-    'Thinternet Login',
-    'Thinternet Database Migration'
+  //dynamic array of contents in shopping Cart -- also on index.js, but sot sur eit is working to recreating here with sample
+  var productList = [
+    'one',
+    'two'
   ];
 
-  // MAJOR TASK #1: DYNAMICALLY CREATE BUTTONS
+  // DYNAMICALLY CREATE BUTTONS
   // =================================================================================
+
   const render = function() {
 
     // 1. Create a for-loop to iterate through the letters array.
-    for( let i = 0; i < projectList.length; i++ ) {
+    for( let i = 0; i < beerList.length; i++ ) {
 
       // 2. Create a variable named "projBtn" equal to $("<button>");
       const projBtn = $('<button>');
@@ -26,34 +21,44 @@ $(document).ready(function() {
       // 3. Then give each "projBtn" the following classes: "proj-button" "proj" "proj-button-color".
       projBtn.addClass('proj-button proj proj-button-color');
 
-      // 4. Then give each "projBtn" an attribute called "data-proj", with a value eqaual to "projectList[i]"
-      projBtn.attr('data-proj', projectList[i]);
+      // 4. Then give each "projBtn" an attribute called "data-proj", with a value eqaual to "beerList[i]"
+      projBtn.attr('data-proj', beerList[i].name);
 
-      // 5. Then give each "projBtn" a text equal to "projectList[i]".
-      projBtn.text(projectList[i]);
+
+      // 5. Then give each "projBtn" a text equal to "beertList[i]".
+      projBtn.text(beerList[i].name);
 
       // 6. Finally, append each "projBtn" to the "#buttons" div (provided).
       $('#buttons').append(projBtn);
+      //console.log(projBtn.'data-proj');
     }
   }
   
   render();
+
+
   // Be sure to test that your code works for this major task, before proceeding to the next one!
 
-  // MAJOR TASK #2: ATTACH ON-CLICK EVENTS TO "PROJECT" BUTTONS
+  //  ATTACH ON-CLICK EVENTS TO "PRODUCT" BUTTONS
   // =================================================================================
   const appendTask = function() {
-
+    
     // Inside the on-click event...
     // 8. Create a variable called "task" and set the variable equal to a new div.
     // 9. Give each "task" the following classes: "proj task-color".
     const task = $('<div>').addClass('proj task-color');
-
+    let beerName = '';
+    //console.log(beerName);
     // 10. Then chain the following code onto the "task" variable: .text($(this).attr("data-proj"))
     task.text($(this).attr("data-proj"));
-
-    // 11. Lastly append the task variable to the "#display" div (provided);
+    beerName = $(this).attr("data-proj");
+    cartContent.push(beerName);
+    console.log(beerName);
+    console.log(cartContent);
+    // 11. Lastly append the task variable to the "#shoppingCart" div;
+    
     $('#shoppingCart').append(task);
+
   }
 
   // 7. Create an "on-click" event attached to the ".proj-button" class.
@@ -61,12 +66,12 @@ $(document).ready(function() {
     
   // Be sure to test that your code works for this major task, before proceeding to the next one!
 
-  // MAJOR TASK #3: ATTACH ON-CLICK EVENTS TO "CLEAR" BUTTON
+  // ATTACH ON-CLICK EVENTS TO "CLEAR" BUTTON to empty the shopping cart
   // =================================================================================
   const clear = function() {
 
     // Inside the on-click event...
-    // 13. Use the jQuery "empty()" method to clear the contents of the "#display" div.
+    // 13. Use the jQuery "empty()" method to clear the contents of the "#shoppingCart" div.
     $('#shoppingCart').empty();
   }
 
---
+++ assets/javascript/index.js
@@ -0,0 +1,27 @@
+//Store list of Beer for sale including catagory
+const beerList = [{
+    name: 'Yuengling',
+    type: 'lager'    
+  },
+  {
+    name: 'Bass Ale',
+    type: 'ale'
+    },
+  {
+    name: 'Sam Adams White Christmas',
+    type: 'ale'
+   },
+  {
+    name: 'UFO White',
+    type: 'ale'
+  },
+  {
+    name: 'Leinenkugel Honey Weiss',
+    type: 'Weiss'
+    },
+  ];
+
+  const cartContent = [ 
+
+  
+];
